bbc65d1d87319160cb7c5e1b24888c02f78720f8
Maria Koutsoukalis <mkoutsoukalis@outlook.com>
2023-08-22 18:03:45 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(10);
